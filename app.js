85fa97d10051854192304ee6b1961388c548ddb0
PhamTruongGiangCK23V7K506 <giangck23v7k506@vlvh.ctu.edu.vn>
2025-03-21 12:52:27 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
 });
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     return res.status(error.statusCode || 500).json({
         message: error.message || "Internal Server Error",
     });
